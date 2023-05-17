762558c3422d7574fbdc7fc7f41bd16e15af2119
Andre Luiz <andrelqvalpassos@fac.pe.senac.br>
2023-05-17 15:09:48 -0300

    first commit

---
+++ simple_server/routes/time.route.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const timeRoutes = express.Router();
+
+let Time = require('../model/Time');
+
+// api to add time
+timeRoutes.route('/add').post(function (req, res) {
+  let time = new Time(req.body);
+  time.save()
+  .then(time => {
+    res.status(200).json({'status': 'success','mssg': 'time added successfully'});
+  })
+  .catch(err => {
+    res.status(409).send({'status': 'failure','mssg': 'unable to save to database'});
+  });
+});
+
+// api to get times
+timeRoutes.route('/').get(function (req, res) {
+  Time.find(function (err, times){
+    if(err){
+      res.status(400).send({'status': 'failure','mssg': 'Something went wrong'});
+    }
+    else {
+      res.status(200).json({'status': 'success','times': times});
+      console.log(times)
+    }
+  });
+});
+
+// api to get time
+timeRoutes.route('/time/:id').get(function (req, res) {
+  let id = req.params.id;
+  Time.findById(id, function (err, time){
+    if(err){
+      res.status(400).send({'status': 'failure','mssg': 'Something went wrong'});
+    }
+    else {
+      res.status(200).json({'status': 'success','time': time});
+    }
+  });
+});
+
+// api to update route
+timeRoutes.route('/update/:id').put(function (req, res) {
+    Time.findById(req.params.id, function(err, time) {
+    if (!time){
+      res.status(400).send({'status': 'failure','mssg': 'Unable to find data'});
+    } else {
+        time.name = req.body.name;
+        time.year = req.body.year;
+        time.historia = req.body.historia;
+
+        time.save().then(business => {
+          res.status(200).json({'status': 'success','mssg': 'Update complete'});
+      })
+    }
+  });
+});
+
+// api for delete
+timeRoutes.route('/delete/:id').delete(function (req, res) {
+  Time.findByIdAndRemove({_id: req.params.id}, function(err,){
+    if(err){
+      res.status(400).send({'status': 'failure','mssg': 'Something went wrong'});
+    }
+    else {
+      res.status(200).json({'status': 'success','mssg': 'Delete successfully'});
+    }
+  });
+});
+
+module.exports = timeRoutes;
---
+++ sw.js
@@ -0,0 +1,58 @@
+const cacheName = 'portfolio'
+
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache){
+            cache.addAll([
+                './',
+                './index.html',
+                './manifest.webmanifest',
+                './index.js',
+                './assets/img/128.png',
+                './assets/img/144.png',
+                './assets/img/152.png',
+                './assets/img/167.png',
+                './assets/img/180.png',
+                './assets/img/192.png',
+                './assets/img/256.png',
+                './assets/img/512.png',
+
+            ])
+        })
+    )
+    return self.skipWaiting()
+})
+
+self.addEventListener('activate', e =>{
+    self.clients.claim()
+})
+
+self.addEventListener('fetch', async e =>{
+    const req = e.request
+    const url = new URL(req.url)
+
+    if(url.origin === location.origin){
+        e.respondWith(cacheFirst(req))
+    } else{
+        e.respondWith(networkAndCache(req))
+    }
+})
+
+async function cacheFirst(req){
+    const cache = await caches.open(cacheName)
+    const cached = await cache.match(req)
+
+    return cached || fetch(req)
+}
+
+async function networkAndCache(req){
+    const cache = await caches.open(cacheName);
+    try{
+        const refresh = await fetch(req)
+        await cache.put(req, fresh.clone())
+        return refresh
+    } catch(e){
+        const cached = await cache.match(req);
+        return cached
+    }
+}
